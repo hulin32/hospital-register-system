f833f502a3c568d82d5471f8014fc00244e6b783
Bene <yuan.sysu@gmail.com>
2015-08-01 16:42:01 +0800

    完成短信验证码模块

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -19,16 +19,16 @@ class UserController extends BaseController{
     public function send_verification_code(){
         Session_start();
 
-        $salt  =  '';   //验证码
-        while( strlen( $salt ) < 6){
-             $salt .= substr( self::$possible_charactors, 
+        $code  =  '';   //验证码
+        while( strlen( $code ) < 6 ){
+             $code .= substr( self::$possible_charactors, 
                               rand( 0, strlen( self::$possible_charactors ) - 1 ),
                               1 );
         }
 
         $user_telephone = Input::get( 'telephone' );
-        
-        if ( preg_match( self::$telephone_reg, $user_telephone )){
+
+        if ( preg_match( self::$telephone_reg, $user_telephone ) ){
 
             $user = User::where( 'phone', $user_telephone )->first();
 
@@ -36,14 +36,36 @@ class UserController extends BaseController{
                 return Response::json(array( 'error_code' => 2, 'message' => '该手机号已被注册' ));
             }
 
-            // TODO: 发送验证码
+            // 发送验证码
+            if ( $this->send_message( $code, $user_telephone ) ){
+                return Response::json(array( 'error_code' => 3, 'message' => '验证码发送失败' ));
+            }
 
-            $_SESSION['verification_code'] = $salt;
+            $_SESSION['verification_code'] = $code;
 
-            return Response::json(array( 'error_code' => 0, 'message' => '验证码已经发送' ))
+            return Response::json(array( 'error_code' => 0, 'message' => '验证码已经发送' ));
         }
 
         return Response::json(array( 'error_code' => 1, 'message' => '手机号码不正确' ));
+    }
+
+    protected function send_message( $code, $user_telephone ){
+        
+        $argv = array(
+            'name' => '13580501456',
+            'pwd' => '1AF3988258DE4FABDD7E1C5FDB36',
+            'sign' => '紫睿科技',
+            'type' => 'pt',
+            'mobile' => $user_telephone,
+            'content' => '您的验证码为：'.$code
+        );
+
+        $url = 'http://web.cr6868.com/asmx/smsservice.aspx?'.http_build_query( $argv, '', '&' );
+        
+        $response = file_get_contents( $url );
+        $return_code = substr( $response, 0, 1 );
+
+        return $return_code == '0';
     }
 
     public function check_verification_code(){
@@ -53,7 +75,7 @@ class UserController extends BaseController{
         $session_code = $_SESSION[ 'verification_code' ];
 
         $validator = Validator::make(
-            array( 'code'  => $input_code ),
+            array( 'code' => $input_code ),
             array( 'code' => 'required|alpha_num|size:6' )
         );
 
@@ -61,7 +83,7 @@ class UserController extends BaseController{
             return Response::json(array( 'error_code' => 2, 'message' => '验证码格式错误' ));
         }
 
-        if ( $input_code ! = $session_code ){
+        if ( $input_code != $session_code ){
             return Response::json(array( 'error_code' => 1, 'message' => '验证码错误' ));
         }
 

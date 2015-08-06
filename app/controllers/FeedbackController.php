9acfdde34005cba1d82f6b1f62dcef3594dca173
hyy <376246826@qq.com>
2015-08-06 19:43:43 +0800

    General update

--- app/controllers/FeedbackController.php
+++ app/controllers/FeedbackController.php
@@ -9,11 +9,11 @@ class FeedbackController extends BaseController{
     public function add_feedback(){
 
         if ( !Input::has( 'title' ) ){
-            return Response::json(array( 'error_code' => 1, 'message' => '请输入标题' ));
+            return Response::json(array( 'error_code' => 2, 'message' => '请输入标题' ));
         }
 
         if ( !Input::has( 'content' ) ){
-            return Response::json(array( 'error_code' => 2, 'message' => '请输入反馈内容' ));
+            return Response::json(array( 'error_code' => 3, 'message' => '请输入反馈内容' ));
         }
 
         $feedback = new Feedback();
@@ -21,7 +21,7 @@ class FeedbackController extends BaseController{
         $feedback->content = Input::get( 'content' );
 
         if ( !$feedback->save() ){
-            return Response::json(array( 'error_code' => 3, 'message' => '反馈失败' ));
+            return Response::json(array( 'error_code' => 1, 'message' => '反馈失败' ));
         }
 
         return Response::json(array( 'error_code' => 0, 'message' => '反馈成功' ));        

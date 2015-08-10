3a84dc72f3610a473f2f63f55d8068ce6a15aae1
hyy <376246826@qq.com>
2015-08-11 07:26:40 +0800

    add pay record route

--- app/routes.php
+++ app/routes.php
@@ -45,6 +45,7 @@ Route::group(array( 'prefix' => 'user' ), function(){
     Route::post( 'login', 'UserController@login_post' );
     Route::post( 'reset_password', 'UserController@verify_and_reset_password' );
 
+    Route::get( 'pay_record', 'UserController@pay_record' );
 	Route::get('center', array('before' => 'auth.user.is_in', 'uses' => 'UserController@user_center'));
 
     // 挂号记录模块

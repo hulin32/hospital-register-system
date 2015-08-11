d5f5c65c151cc3423dfdb029f9caaf826c10c214
hyy <376246826@qq.com>
2015-08-11 16:43:34 +0800

    Add remember user function

--- app/views/user/login.blade.php
+++ app/views/user/login.blade.php
@@ -24,11 +24,12 @@
 
             $('.login-form').on( 'submit', function( event ){
                 event.preventDefault();
+                alert( $(this).serialize() );
                 $.ajax({
                     url: '/user/login',
                     type: 'POST',
                     dataType: 'json',
-                    data: $(this).serialize(),
+                    data: $('.login-form').serialize(),
                     success: function ( json ){
                         alert( json.message );
                         if ( json.error_code == 0 ){
@@ -47,12 +48,16 @@
 
 @section('body-main')
     <form class="login-form" method="post" action="/user/login">
-        <input class="input-blk form-blk" placeholder="手机号码" id="phone" name="phone" type="text">
-        <input class="input-blk form-blk" placeholder="密码" id="password" name="password" type="password">
+        <input class="input-blk form-blk" placeholder="手机号码" id="phone" name="phone" type="text" value="{{{ $remember_phone }}}">
+        <input class="input-blk form-blk" placeholder="密码" id="password" name="password" type="password" value="{{{ $remember_password }}}">
         <div class="form-mdl form-blk clearfix">
-            <input id="rem-check" class="checkbox" type="checkbox">
+            @if ( isset( $remember ) && $remember == 'true' )
+            <input id="rem-check" class="checkbox" type="checkbox" name="remember" value="true" checked="checked">
+            @else
+            <input id="rem-check" class="checkbox" type="checkbox" name="remember" value="true">
+            @endif
             <span class="checkbox-text">记住用户名</span>
-            <a class="recover-pwd" href="recover_password.php">找回密码 ></a>
+            <a class="recover-pwd" href="#">找回密码 ></a>
         </div>
         <input class="btn form-blk" type="submit" value="登陆">
     </form>
